e8b6dd8a55e5a56b23a3bb4a470cd6d70e40dbaf
Furushchev <furushchev@mail.ru>
2013-10-09 00:45:48 +0900

    use mongodb

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,6 +1,7 @@
 $(function() {
 	socket = io.connect('http://localhost');
 	socket.on('connect', function() {
+		socket.emit('msg update');
 		console.log('connected.');
 	});
 
@@ -18,14 +19,31 @@ $(function() {
 		message.val('');
 	});
 
+	$('#delete').click(function(){
+		if(window.confirm("Are you sure?") == true) {
+			socket.emit('deleteDB');
+			$('#list').empty();
+		}
+	});
+
+	socket.on('msg open', function(data) {
+		if(data.length == 0) {
+			console.log("nothing to load.");
+			return;
+		} else {
+			console.log(data);
+			$('#list').empty();
+			$.each(data, function(key, value) {
+				console.log("hoge");
+				$('#list').prepend($('<dt>@' + value.name + ': ' + value.date + '</dt><dd>' + value.markdown + '</dd>'));
+			});
+		}
+	});			  
+
 	socket.on('msg push', function(data) {
 		console.log(data);
 		var date = new Date();
 		$('#list').prepend($('<dt>@' + data.name + ': ' + date + '</dt><dd>' + data.markdown + '</dd>'));
 	});
 
-	socket.on('msg updateDB', function(data) {
-		// TODO: mongoDBにつっこむ予定
-		console.log(data);
-	});
 });
