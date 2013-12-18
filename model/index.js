ea5edf6aeac37039dfc12b45e32e29f878a8c428
garaemon <garaemon@gmail.com>
2013-12-18 03:45:54 -0500

    #24: adding User model

--- model/index.js
+++ model/index.js
@@ -1,6 +1,6 @@
 // model/index.js
 
-var files = ["./say.js"]
+var files = ["./say.js", "./user.js"]
 
 for (var i = 0; i < files.length; i++) {
     require(files[i]);
---
+++ model/user.js
@@ -0,0 +1,17 @@
+/**
+ * User model
+ */
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  nick_name: {type: String, required: true, unique: true},
+  email: {type: String},
+  password: {type: String, required: true}
+});
+
+mongoose.model('User', UserSchema);
+
+var User = mongoose.model('User');
