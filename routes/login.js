12443439edcffef34481a6a4e15006838256d991
garaemon <garaemon@gmail.com>
2013-12-18 10:16:40 -0500

    #24: " -> '

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 // routes/login.js
-var config = require("../config");
-exports.get_url = "/login";
-exports.get = function(req, res){
+var config = require('../config');
+exports.get_url = '/login';
+exports.get = function(req, res) {
   res.render('login', { title: config.TITLE });
 };
--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
 // routes/login.js
-var config = require("../config");
-exports.get_url = "/register";
+var config = require('../config');
+exports.get_url = '/register';
 exports.get = function(req, res){
   res.render('register', { title: config.TITLE });
 };
