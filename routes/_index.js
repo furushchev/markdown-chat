a5c5c534730d65782eb56a54e581a7336d5accb3
garaemon <garaemon@gmail.com>
2013-12-21 14:12:55 -0500

    #24: automatically insert name if logged in

--- routes/_index.js
+++ routes/_index.js
@@ -5,5 +5,15 @@ var config = require("../config");
 
 exports.get_url = "/";
 exports.get = function(req, res){
-  res.render('index', { title: config.TITLE });
+  if (req.isAuthenticated()) {
+    res.render('index', {
+      title: config.TITLE,
+      logged_in: true,
+      nickname: req.user.nickname
+    });
+  }
+  else {
+    res.render('index', { title: config.TITLE,
+                          logged_in: false});
+  }
 };
