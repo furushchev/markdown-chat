9f63435f0bb4adc3b5c8ede4ffafdf7801ccc54f
garaemon <garaemon@gmail.com>
2013-12-21 14:09:07 -0500

    #24: implementing logout

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-var _ = require("lodash");
-var files = ["_index", "say", "page", "search", "login", "register"];
+var _ = require('lodash');
+var files = ['_index', 'say', 'page', 'search', 'login', 'register', 'logout'];
 
 exports.routes = _.map(files, function(f) {
-    return require("./" + f);
+    return require('./' + f);
 });
