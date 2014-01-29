b44f872eb315822d503472a08c9eeea85aa0074b
garaemon <garaemon@gmail.com>
2014-01-29 11:05:07 +0900

    #27: error handling if the user cannot be found

--- routes/user.js
+++ routes/user.js
@@ -3,26 +3,38 @@ var q = require("q");
 
 exports.get_url = "/user/:id";
 
-exports.get = function(req, res) {
+exports.get = function(req, res, next) {
   var user_id = req.params.id;
   var Say = mongoose.model("Say");
-  Say.find({user: user_id}, function(err, says) {
-    if (!says) {
-      says = [];
+  var User = mongoose.model("User");
+  User.findById(user_id, function(err, user) {
+    if (err != null) {
+      next(err);
     }
-    q.allSettled(says.map(function(say) {
-      return say.renderMarkdown();
-    }))
-    .then(function(results) {
-      res.render("user", {
-        title: "MarkdownChat",
-        logged_in: req.isAuthenticated(),
-        htmls: results.map(function(r) {
-          return r.value;
-        })
+    else if (user == null) {
+      next(404);
+    }
+    else { 
+      Say.find({user: user_id}, function(err, says) {
+        if (!says) {
+          says = [];
+        }
+        q.allSettled(says.map(function(say) {
+          return say.renderMarkdown();
+        }))
+          .then(function(results) {
+            res.render("user", {
+              title: "MarkdownChat",
+              logged_in: req.isAuthenticated(),
+              nickname: (req.user || {}).nickname,
+              user_id: (req.user || {})._id,
+              user_name: user.nickname,
+              htmls: results.map(function(r) {
+                return r.value;
+              })
+            });
+          });
       });
-    });
+    }
   });
-  // console.log(user_id);
-  // res.send(200);
 };
