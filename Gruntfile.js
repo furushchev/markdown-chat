71ce7b46590bc05cf7c4516e45598cbcc7ff7d89
Ryohei Ueda <garaemon@gmail.com>
2014-03-04 13:12:10 +0900

    #50: add Gruntfile and update package.json to install grunt
    add Gruntfile.js to run grunt in travis.yml
    add several packages to run gruntfile which includes the packages
    that are not used yet

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  var pkg = grunt.file.readJSON('package.json');
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      files: [
+        'Gruntfile.js',
+        './model/*.js',
+        './routes/*.js',
+        '*.js'
+      ]
+    },
+    watch: {
+      files: ['./**/*.js', 'Gruntfile.js', '.jshintrc'],
+      tasks: ['build']
+    }
+  });
+
+  // reading npm tasks
+  for(var taskName in pkg.devDependencies) {
+    if(taskName.substring(0, 6) === 'grunt-'.toString()) {
+      grunt.loadNpmTasks(taskName);
+    }
+  }
+  
+  grunt.registerTask('build', ['jshint']);
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('doc', ['jsdoc']);
+};
