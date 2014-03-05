e8bdb91fac4f783f91bcae4f0e824bd1686d0a0e
Ryohei Ueda <garaemon@gmail.com>
2014-03-05 15:57:40 +0900

    #50: fix coding style of model/index.js according to jshintrc

--- model/index.js
+++ model/index.js
@@ -1,8 +1,8 @@
 // model/index.js
 
-var files = ["./say.js", "./user.js"]
+var files = ['say.js', 'user.js'];
 
 for (var i = 0; i < files.length; i++) {
-  require(files[i]);
+  require('./' + files[i]);
 }
 
