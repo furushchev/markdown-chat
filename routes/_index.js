a5923184cf6bed2b375e96da4cf94e00ccc26604
Ryohei Ueda <garaemon@gmail.com>
2014-03-05 15:53:47 +0900

    #50: fix coding style of routest/_index.js according to jshint

--- routes/_index.js
+++ routes/_index.js
@@ -1,13 +1,13 @@
 /*
  * GET home page.
  */
-var config = require("../config");
+var config = require('../config');
 
-exports.get_url = "/";
+exports.get_url = '/';
 exports.get = function(req, res){
   if (req.isAuthenticated()) {
     res.render('index', {
-      title: process.env.MD_TITLE || "Markdown Chat",
+      title: process.env.MD_TITLE || 'Markdown Chat',
       logged_in: true,
       nickname: req.user.nickname,
       user_id: req.user._id
@@ -15,7 +15,7 @@ exports.get = function(req, res){
   }
   else {
     res.render('index', {
-      title: process.env.MD_TITLE || "Markdown Chat",
+      title: process.env.MD_TITLE || 'Markdown Chat',
       logged_in: false
     });
   }
